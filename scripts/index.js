411e6f780dd2432012989dbde349f9a7e20085a3
Artem Cherepanov <nf11082016@gmail.com>
2022-02-09 23:53:32 +0500

    09.02.2022

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 const profileOpenPopupButton = document.querySelector('.profile__edit-button')
+const popupAddBtn = document.querySelector('.profile__add-button')
 
 const popupEdit = document.querySelector('.popup-edit')
-const popupCloseButton = document.querySelector('.popup__close-button')
-const popupAddBtn = document.querySelector('.profile__add-button')
+const popupCloseButtonEdit = popupEdit.querySelector('.popup__close-button')
 
 const popupAdd = document.querySelector('.popup-add-element')
 const titleInputValue = popupAdd.querySelector('.popup__input-name')
 const descriptionInputValue = popupAdd.querySelector('.popup__input-about')
-const popupCloseBtn = popupAdd.querySelector('.popup__close-button')
+const popupCloseButtonAdd = popupAdd.querySelector('.popup__close-button')
 const popupFormSubmitAdd = popupAdd.querySelector('.popup__form')
 
 const formElement = document.querySelector('.popup__container')
@@ -26,32 +26,6 @@ const popupBtnAddElementsClose = popupOpenElements.querySelector('.popup__close-
 const sectionElements = document.querySelector('.elements')
 const elementTemplate = document.querySelector('.temlate').content
 
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-]; 
 
 function openPopup(popup) {
   popup.classList.add('popup_opened')
@@ -67,7 +41,7 @@ profileOpenPopupButton.addEventListener('click', function() {
   aboutInput.value = profileAbout.textContent
 })
 
-popupCloseButton.addEventListener('click', function(){
+popupCloseButtonEdit.addEventListener('click', function(){
   closePopup(popupEdit)
 })
 
@@ -75,24 +49,25 @@ popupAddBtn.addEventListener('click', function(){
   openPopup(popupAdd)
 })
 
-popupCloseBtn.addEventListener('click', function(){
+popupCloseButtonAdd.addEventListener('click', function(){
   closePopup(popupAdd)
 })
 
-function formSubmitHandler (evt) {
+function handleProfileFormSubmit (evt) {
   evt.preventDefault()
   profileName.textContent = nameInput.value
   profileAbout.textContent = aboutInput.value
   closePopup(popupEdit)
 }
-formElement.addEventListener('submit', formSubmitHandler)
+formElement.addEventListener('submit', handleProfileFormSubmit)
 
 function createCard(name, link) {
     const card = elementTemplate.querySelector('.element').cloneNode(true)
+    const elementInfoCaption = card.querySelector('.element__info-caption')
     const elementImage = card.querySelector('.element__image')
-    card.querySelector('.element__info-caption').textContent = name
-    card.querySelector('.element__image').src = link
-    let buttonLike = document.querySelector('.element__info-button')
+    elementInfoCaption.textContent = name
+    elementImage.src = link
+    elementImage.alt = name
     card
     .querySelector('.element__info-button')
     .addEventListener('click', function(evt) {
@@ -101,32 +76,29 @@ function createCard(name, link) {
     elementImage.addEventListener('click', function(){
       openPopup(popupOpenElements)
       imagePopupPreview.src = link
+      imagePopupPreview.alt = name
       titlePopupPreview.textContent = name
     })
     card.querySelector('.element__button-element-detele').addEventListener('click', function(evt) {
       evt.target.closest('.element').remove()
     })
-    popupBtnAddElementsClose.addEventListener('click', function(){
-      closePopup(popupOpenElements)
-    })
     return card;
 }
 
-function render(card) {
-    sectionElements.append(card)
-}
-
-initialCards.forEach((item) => {
-    let cardItem = createCard(item.name, item.link)
-    render(cardItem)
+popupBtnAddElementsClose.addEventListener('click', function(){
+closePopup(popupOpenElements)
 })
 
-function formSubmitHandlerAdd (evt) {
-  evt.preventDefault();
-  sectionElements.prepend(createCard(titleInputValue.value, descriptionInputValue.value));
-  closePopup(popupAdd);
-  titleInputValue.value = '';
-  descriptionInputValue.value = '';
+initialCards.forEach(function(item) {
+  sectionElements.prepend(createCard(item.name, item.link));
+})
+
+function handleCardFormSubmit (evt) {
+  evt.preventDefault()
+  sectionElements.prepend(createCard(titleInputValue.value, descriptionInputValue.value))
+  closePopup(popupAdd)
+  titleInputValue.reset()
+  descriptionInputValue.reset()
 }
 
-popupFormSubmitAdd.addEventListener('submit', formSubmitHandlerAdd);
+popupFormSubmitAdd.addEventListener('submit', handleCardFormSubmit);
